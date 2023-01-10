3688d26bcb65e70cd578eae10eb841d015caeeaf
allachevskaya <alprel@list.ru>
2023-01-10 20:45:18 +0300

    start admin

--- angular-app-Kalmyl-Wool/src/app/pages/login/login.component.ts
+++ angular-app-Kalmyl-Wool/src/app/pages/login/login.component.ts
@@ -13,16 +13,24 @@ export class LoginComponent implements OnInit {
   _password?:string;
   userPassword?: any;
   userLogin?: any;
+  openAdmin?:boolean;
 
   ngOnInit(): void {
     this._login = 'admin'
     this._password = '123'
+    this.openAdmin = false
   }
   @ViewChild('testForm') someNewNameFrom: NgForm | null = null;
   
 
   onSubmit() {
     console.log(this.someNewNameFrom?.value); //получила значение из инпута
+    //console.log(this.someNewNameFrom?.value.userLogin) // чтобы считать только значение которое заносится в userLogin
+    if(this.someNewNameFrom?.value.userLogin === this._login && this.someNewNameFrom?.value.userPassword === this._password){
+      this.openAdmin = true;
+    }
   }
 
 }
+
+
