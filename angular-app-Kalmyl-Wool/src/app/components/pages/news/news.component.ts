7d561baae48ba458204a938ce32530c92df5998c
allachevskaya <alprel@list.ru>
2023-01-12 13:43:27 +0300

    input toggleAdmin

--- angular-app-Kalmyl-Wool/src/app/components/pages/news/news.component.ts
+++ angular-app-Kalmyl-Wool/src/app/components/pages/news/news.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
-export class NewsComponent {
+export class NewsComponent implements  OnChanges {
+
+
+  @Input() toggleAdmin: boolean = false; 
+
+  
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(this.toggleAdmin)
+  }
+
+  
+
+
+
+  
 
 }
