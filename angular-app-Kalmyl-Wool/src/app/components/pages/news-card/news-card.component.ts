6db9d98c5f5eccbaf8204f52825a6ddf5f62b481
allachevskaya <alprel@list.ru>
2023-01-30 13:02:17 +0300

    add link

--- angular-app-Kalmyl-Wool/src/app/components/pages/news-card/news-card.component.ts
+++ angular-app-Kalmyl-Wool/src/app/components/pages/news-card/news-card.component.ts
@@ -12,64 +12,55 @@ export class NewsCardComponent implements OnInit {
       title:string;
       dectiption:string;
       date:string;
+      link:string;
   }[] | undefined;
 
 
   ngOnInit(): void {
     this.cards = [
       {
-        'img': 'https://images.unsplash.com/photo-1484557985045-edf25e08da73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80',
-        'title': 'Новость 1',
-        'dectiption': 'Компания Калмык Вул является единственной организацией в Республике Калмыкия, которая экспортирует шерсть в 5 стран мира!',
-        'date': '10.10.22',
+        'img': 'assets/images/news/news1.png',
+        'title': 'Вести Калмыкия',
+        'dectiption': '20 тонн калмыцкой овечьей шерсти отправилось в Индию',
+        'date': '19.10.21',
+        'link':'https://vesti-kalmykia.ru/news/20-tonn-kalmyckoj-ovechej-shersti-otpravilos-v-indiyu',
       },
       {
-        'img': 'https://images.unsplash.com/photo-1484557985045-edf25e08da73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80',
-        'title': 'Новость 1',
-        'dectiption': 'Компания Калмык Вул является единственной организацией в Республике Калмыкия, которая экспортирует шерсть в 5 стран мира!',
-        'date': '10.10.22',
+        'img': 'assets/images/news/news2.png',
+        'title': 'Калмыцкая шерсть ',
+        'dectiption': 'Калмыцкая шерсть укрепляет позиции на международном рынке',
+        'date': '10.11.22',
+        'link':'https://youtu.be/FsDewIDrwfQ',
       },
       {
-        'img': 'https://images.unsplash.com/photo-1484557985045-edf25e08da73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80',
-        'title': 'Новость 1',
+        'img': 'assets/images/news/news3.png',
+        'title': 'В Калмыкии открылся Центр по подготовке и сортировке шерсти',
         'dectiption': 'Компания Калмык Вул является единственной организацией в Республике Калмыкия, которая экспортирует шерсть в 5 стран мира!',
-        'date': '10.10.22',
+        'date': '27.17.22',
+        'link':'https://www.youtube.com/watch?v=QSFhE7N3KlI',
       },
       {
-        'img': 'https://images.unsplash.com/photo-1484557985045-edf25e08da73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80',
-        'title': 'Новость 1',
-        'dectiption': 'Компания Калмык Вул является единственной организацией в Республике Калмыкия, которая экспортирует шерсть в 5 стран мира!',
-        'date': '10.10.22',
+        'img': 'assets/images/news/news4.png',
+        'title': 'Отгрузка шерсти в Индию ',
+        'dectiption': '20 тонн калмыцкой шерсти отправилось в Индию',
+        'date': '19.10.22',
+        'link':'https://www.youtube.com/watch?v=kU1vmT6EhfQ',
       },
       {
-        'img': 'https://images.unsplash.com/photo-1484557985045-edf25e08da73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80',
-        'title': 'Новость 1',
-        'dectiption': 'Компания Калмык Вул является единственной организацией в Республике Калмыкия, которая экспортирует шерсть в 5 стран мира!',
-        'date': '10.10.22',
+        'img': 'assets/images/news/news5.png',
+        'title': 'Компания «КАЛМЫК ВУЛ» расширяет географию экспорта шерсти',
+        'dectiption': '«Калмык Вул» – ведущий экспортер региона, занимающийся первичной переработкой и продажей шерсти.',
+        'date': '30.08.22',
+        'link':'https://riakalm.ru/news2/34907-kompaniya-kalmyk-vul-rasshiryaet-geografiyu-eksporta-shersti',
       },
       {
-        'img': 'https://images.unsplash.com/photo-1484557985045-edf25e08da73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80',
-        'title': 'Новость 1',
-        'dectiption': 'Компания Калмык Вул является единственной организацией в Республике Калмыкия, которая экспортирует шерсть в 5 стран мира!',
-        'date': '10.10.22',
-      }, {
-        'img': 'https://images.unsplash.com/photo-1484557985045-edf25e08da73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80',
-        'title': 'Новость 1',
-        'dectiption': 'Компания Калмык Вул является единственной организацией в Республике Калмыкия, которая экспортирует шерсть в 5 стран мира!',
-        'date': '10.10.22',
+        'img': 'assets/images/news/news6.png',
+        'title': 'Предприятие «Kalmyk Wool» расширит географию экспорта',
+        'dectiption': '«Калмык Вул» – ведущий экспортер региона, занимающийся первичной переработкой и продажей шерсти.',
+        'date': '26.06.22',
+        'link':'https://youtu.be/Fhz4E21DnLs',
       },
-      {
-        'img': 'https://images.unsplash.com/photo-1484557985045-edf25e08da73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80',
-        'title': 'Новость 1',
-        'dectiption': 'Компания Калмык Вул является единственной организацией в Республике Калмыкия, которая экспортирует шерсть в 5 стран мира!',
-        'date': '10.10.22',
-      },
-      {
-        'img': 'https://images.unsplash.com/photo-1484557985045-edf25e08da73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80',
-        'title': 'Новость 1',
-        'dectiption': 'Компания Калмык Вул является единственной организацией в Республике Калмыкия, которая экспортирует шерсть в 5 стран мира!',
-        'date': '10.10.22',
-      },
+      
   
     ];
 
