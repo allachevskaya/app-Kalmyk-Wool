aa76c1bbea35d7b2421268559a278322e364dfc5
allachevskaya <alprel@list.ru>
2023-02-23 11:10:22 +0300

    changes fetch

--- angular-app-Kalmyl-Wool/src/app/services/general.service.ts
+++ angular-app-Kalmyl-Wool/src/app/services/general.service.ts
@@ -5,24 +5,25 @@ import { Injectable } from '@angular/core';
 })
 export class GeneralService {
 
-  api = 'https://admin.kalmykwool.com'
+  api = 'https://admin.kalmykwool.com';
+  //https://admin.kalmykwool.com/api/news-cards?populate=*&locale=ru&sort[0]=date%3Adesc
 
   constructor() { }
 
   async renderStaff() {
-    const responseStaff = await fetch(this.api +'/api/staffs?populate=*&locale=ru&sort[0]=updatedAt%3Aasc%3Adesc');
+    const responseStaff = await fetch(this.api +'/api/staffs?populate=*&locale=ru&sort[0]=updatedAt%3Aasc');
     const respDataStaff = await responseStaff.json();
     const { data } = respDataStaff;
     return data;
   }
   async renderStaff_en() {
-    const responseStaff = await fetch(this.api + '/api/staffs?populate=*&locale=en&sort[0]=updatedAt%3Aasc%3Adesc');
+    const responseStaff = await fetch(this.api + '/api/staffs?populate=*&locale=en&sort[0]=updatedAt%3Aasc');
     const respDataStaff = await responseStaff.json();
     const { data } = respDataStaff;
     return data;
   }
   async renderStaff_ch() {
-    const responseStaff = await fetch(this.api + '/api/staffs?populate=*&locale=zh&sort[0]=updatedAt%3Aasc%3Adesc');
+    const responseStaff = await fetch(this.api + '/api/staffs?populate=*&locale=zh&sort[0]=updatedAt%3Aasc');
     const respDataStaff = await responseStaff.json();
     const { data } = respDataStaff;
     return data;
@@ -48,26 +49,26 @@ export class GeneralService {
   }
 
   async renderAssortment() {
-    const responseAssortment = await fetch(this.api + '/api/assortiments?populate=*&locale=ru&sort[0]=updatedAt%3Aasc%3Adesc');
+    const responseAssortment = await fetch(this.api + '/api/assortiments?populate=*&locale=ru&sort[0]=updatedAt%3Aasc');
     const respDataAssortment = await responseAssortment.json();
     const { data } = respDataAssortment;
     return data;
   }
   async renderAssortment_en() {
-    const responseAssortment = await fetch(this.api +'/api/assortiments?populate=*&locale=en&sort[0]=updatedAt%3Aasc%3Adesc');
+    const responseAssortment = await fetch(this.api +'/api/assortiments?populate=*&locale=en&sort[0]=updatedAt%3Aasc');
     const respDataAssortment = await responseAssortment.json();
     const { data } = respDataAssortment;
     return data;
   }
   async renderAssortment_ch() {
-    const responseAssortment = await fetch(this.api +'/api/assortiments?populate=*&locale=zh&sort[0]=updatedAt%3Aasc%3Adesc');
+    const responseAssortment = await fetch(this.api +'/api/assortiments?populate=*&locale=zh&sort[0]=updatedAt%3Aasc');
     const respDataAssortment = await responseAssortment.json();
     const { data } = respDataAssortment;
     return data;
   }
 
   async renderDetailed() {
-    const response = await fetch(this.api +'/api/detaileds?populate=*&locale=ru&sort[0]=updatedAt%3Aasc%3Adesc');
+    const response = await fetch(this.api +'/api/detaileds?populate=*&locale=ru&sort[0]=updatedAt%3Aasc');
     // const response = await fetch ('pagination[page]=1&pagination[pageSize]=10')
     const respData = await response.json();
     const { data } = respData;
@@ -75,32 +76,32 @@ export class GeneralService {
   }
 
   async renderDetailed_en() {
-    const response = await fetch(this.api +'/api/detaileds?populate=*&locale=en&sort[0]=updatedAt%3Aasc%3Adesc');
+    const response = await fetch(this.api +'/api/detaileds?populate=*&locale=en&sort[0]=updatedAt%3Aasc');
     const respData = await response.json();
     const { data } = respData;
     return data;
   }
   async renderDetailed_ch() {
-    const response = await fetch(this.api +'/api/detaileds?populate=*&locale=zh&sort[0]=updatedAt%3Aasc%3Adesc');
+    const response = await fetch(this.api +'/api/detaileds?populate=*&locale=zh&sort[0]=updatedAt%3Aasc');
     const respData = await response.json();
     const { data } = respData;
     return data;
   }
 
   async renderCard() {
-    const response = await fetch(this.api +'/api/news-cards?populate=*&locale=ru&sort[1]=updatedAt%3Aasc%3Adesc');///////////
+    const response = await fetch(this.api +'api/news-cards?populate=*&locale=ru&sort[0]=date%3Adesc');///////////
     const respData = await response.json();
     const { data } = respData;
     return data;
   }
   async renderCard_en() {
-    const response = await fetch(this.api +'/api/news-cards?populate=*&locale=en&sort[0]=updatedAt%3Aasc%3Adesc');
+    const response = await fetch(this.api +'api/news-cards?populate=*&locale=en&sort[0]=date%3Adesc');
     const respData = await response.json();
     const { data } = respData;
     return data;
   }
   async renderCard_ch() {
-    const response = await fetch(this.api +'/api/news-cards?populate=*&locale=zh&sort[0]=updatedAt%3Aasc%3Adesc');
+    const response = await fetch(this.api +'api/news-cards?populate=*&locale=zh&sort[0]=date%3Adesc');
     const respData = await response.json();
     const { data } = respData;
     return data;
